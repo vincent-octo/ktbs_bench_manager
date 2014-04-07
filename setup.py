4b8d9ab11ea904d2bf64530a03b971540e6bfe9e
vincent <vincent-octo@users.noreply.github.com>
2014-04-07 15:42:33 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ktbs_bench_manager",
+    version="0.0.1",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['rdflib',
+                      'sutils'],
+
+    # metadata for upload to PyPI
+    author="Vincent",
+    author_email="vincent-octo@users.noreply.github.com",
+    description="kTBS bench manager",
+    license="MIT",
+    url="https://github.com/vincent-octo/ktbs_bench_manager",  # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
