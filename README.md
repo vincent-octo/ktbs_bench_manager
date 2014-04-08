9b5b405cf83920c644d4916dc8a13e94cef5f6d9
vincent <vincent-octo@users.noreply.github.com>
2014-04-08 10:54:04 +0200

    Update project information

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-ktbs_bench_manager
+kTBS Bench Manager
 ==================
+
+Provide object-oriented tools that extend [sutils](https://github.com/vincent-octo/sutils) for benchmarking the [kTBS](http://liris.cnrs.fr/sbt-dev/ktbs).
+
+
+License
+-------
+
+MIT, see [LICENSE](LICENSE).
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=['rdflib==4.1.1',
                       'sutils'],
 
-    dependency_links=['https://github.com/vincent-octo/sutils/tarball/master#egg=sutils-master'],
+    dependency_links=['https://github.com/vincent-octo/sutils/tarball/master#egg=sutils'],
 
     extras_require={'tests': ['pytest']},
 
